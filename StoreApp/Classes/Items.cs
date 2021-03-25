d1826b137abba7a966e245b70ce14189bda2c4c9
hostmahmud <44112100+hostmahmud@users.noreply.github.com>
2021-03-25 13:50:08 +0600

    upload master

---
+++ StoreApp/Classes/Items.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Classes
+{
+    class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Upc { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public string Category { get; set; }
+        public string IsStock { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ StoreApp/Classes/Sales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Classes
+{
+    class Sales
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string CustName { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string Status { get; set; }
+
+    }
+}
